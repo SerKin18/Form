bd8c84ec08f4bb7c6c312e3228821401ea545a50
Sergey <sergeykinyak@gmail.com>
2023-10-18 22:55:33 +0300

    form by component Formik

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './app.scss';
-import Form from './Form';
+import Forms from './Form';
 
 function App() {
   return (
     <div className="app">
-        <Form/>
+        <Forms/>
     </div>
   );
 }
