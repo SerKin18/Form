7434bed14edbf5037d6cd95157a2814b2887aa97
Sergey <sergeykinyak@gmail.com>
2023-10-18 23:14:16 +0300

    Formik useField

--- src/Form.js
+++ src/Form.js
@@ -1,87 +1,81 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, useField } from "formik";
 import * as Yup from "yup";
 
+const MyTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <>
+      <label htmlFor={props.name}>{label}</label>
+      <input {...props} {...field} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
 const Forms = () => {
+  return (
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        amount: 0,
+        currency: "",
+        text: "",
+        terms: false,
+      }}
+      validationSchema={Yup.object({
+        name: Yup.string()
+          .min(3, "Миниму 3 символа")
+          .required("Обязательное поле!"),
+        email: Yup.string()
+          .email("Неправильный email адрес")
+          .required("Обязательное поле!"),
+        amount: Yup.number()
+          .min(5, "Не менее 5")
+          .required("Обязательное поле!"),
+        currency: Yup.string().required("Выберите валюту!"),
+        text: Yup.string()
+          .min(10, "Минимум 10 символов")
+          .required("Обязательное поле!"),
+        terms: Yup.boolean()
+          .required("Необходимо согласие!")
+          .oneOf([true], "Необходимо согласие!"),
+      })}
+      onSubmit={(values) => console.log(JSON.stringify(values, null, 2))}
+    >
+      <Form className="form">
+        <h2>Отправить пожертвование</h2>
+        <MyTextInput label="Ваше имя" id="name" name="name" type="text" />
 
+        <MyTextInput label="Ваша почта" id="email" name="email" type="email" />
 
-	return (
-		<Formik initialValues={{
-			name: "",
-			email: "",
-			amount: 0,
-			currency: "",
-			text: "",
-			terms: false,
-		}}
-			validationSchema={Yup.object({
-				name: Yup.string()
-					.min(3, "Миниму 3 символа")
-					.required("Обязательное поле!"),
-				email: Yup.string()
-					.email("Неправильный email адрес")
-					.required("Обязательное поле!"),
-				amount: Yup.number().min(5, "Не менее 5").required("Обязательное поле!"),
-				currency: Yup.string().required("Выберите валюту!"),
-				text: Yup.string().min(10, "Минимум 10 символов").required("Обязательное поле!"),
-				terms: Yup.boolean()
-					.required("Необходимо согласие!")
-					.oneOf([true], "Необходимо согласие!"),
-			})}
+        <label htmlFor="amount">Количество</label>
+        <Field id="amount" name="amount" type="number" />
+        <ErrorMessage className="error" name="amount" component="div" />
 
-			onSubmit={(values) => console.log(JSON.stringify(values, null, 2))}>
-			<Form className="form" >
-				<h2>Отправить пожертвование</h2>
-				<label htmlFor="name">Ваше имя</label>
-				<Field
-					id="name"
-					name="name"
-					type="text"
-				/>
-				<ErrorMessage className='error' name="name" component="div" />
-				<label htmlFor="email">Ваша почта</label>
-				<Field
-					id="email"
-					name="email"
-					type="email"
-				/>
-				<ErrorMessage className='error' name="email" component="div" />
-				<label htmlFor="amount">Количество</label>
-				<Field
-					id="amount"
-					name="amount"
-					type="number"
-				/>
-				<ErrorMessage className='error' name="amount" component="div" />
-				<label htmlFor="currency">Валюта</label>
-				<Field
-					id="currency"
-					name="currency"
-					as="select">
-					<option value="">Выберите валюту</option>
-					<option value="USD">USD</option>
-					<option value="UAH">UAH</option>
-					<option value="RUB">RUB</option>
-				</Field>
-				<ErrorMessage className='error' name="currency" component="div" />
-				<label htmlFor="text">Ваше сообщение</label>
-				<Field
-					id="text"
-					name="text"
-					as="textarea"
-				/>
-				<ErrorMessage className='error' name="text" component="div" />
-				<label className="checkbox">
-					<Field
-						name="terms"
-						type="checkbox"
-					/>
-					Соглашаетесь с политикой конфиденциальности?
-				</label>
-				<ErrorMessage className='error' name="terms" component="div" />
-				<button type="submit">Отправить</button>
-			</Form>
-		</Formik>
-	);
+        <label htmlFor="currency">Валюта</label>
+        <Field id="currency" name="currency" as="select">
+          <option value="">Выберите валюту</option>
+          <option value="USD">USD</option>
+          <option value="UAH">UAH</option>
+          <option value="RUB">RUB</option>
+        </Field>
+        <ErrorMessage className="error" name="currency" component="div" />
+        <label htmlFor="text">Ваше сообщение</label>
+        <Field id="text" name="text" as="textarea" />
+        <ErrorMessage className="error" name="text" component="div" />
+        <label className="checkbox">
+          <Field name="terms" type="checkbox" />
+          Соглашаетесь с политикой конфиденциальности?
+        </label>
+        <ErrorMessage className="error" name="terms" component="div" />
+        <button type="submit">Отправить</button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default Forms;
